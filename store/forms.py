3f1e5689d01275458ea02064c4ecd718a0a0dd14
jvnne-onti <jann130608@gmail.com>
2025-11-21 09:47:18 -0800

    Create NewItemForm for item submission
    Added NewItemForm for creating new items with specified fields and widgets.

--- store/forms.py
+++ store/forms.py
@@ -51,3 +51,29 @@ class SignupForm(UserCreationForm):
             'class': 'form-control'
         }
     ))
+    class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image',)
+
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'style': 'height: 100px'
+            }),
+            'price': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'price': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'image': forms.FileInput(attrs={
+                'class': 'form-control',
+            }),
+        }
